501e394805714c028245a4911ac576b42cfadded
Shaheer cp <shaheercp890@gmail.com>
2025-11-20 09:49:51 +0530

    feat(frontend) :enviroment for production

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://zukasports.store/api/v1',
+  socketUrl: 'https://zukasports.store',
+  STRIPE_PUBLISHABLE_KEY:
+    'pk_test_51SEMUcFg0fgJFs3kw19qvAQFFZUxStDRHmuLo7xIrziDkdAO7laGQxa6knN0JvftIrf5gJM0HneBbnoJhD2VLCBb00xx8I9CKy',
+};
