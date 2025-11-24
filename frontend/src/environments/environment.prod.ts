1e4495f80656b85b007467df1787fe9988853c08
Shaheer cp <shaheercp890@gmail.com>
2025-11-24 10:08:49 +0530

    fix(frontend) :Frontend url changed

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://zukasports.store/api/v1',
-  socketUrl: 'https://zukasports.store',
+  apiUrl: 'http://56.228.26.151/api/v1',
+  socketUrl: 'http://56.228.26.151/',
   STRIPE_PUBLISHABLE_KEY:
     'pk_test_51SEMUcFg0fgJFs3kw19qvAQFFZUxStDRHmuLo7xIrziDkdAO7laGQxa6knN0JvftIrf5gJM0HneBbnoJhD2VLCBb00xx8I9CKy',
 };
